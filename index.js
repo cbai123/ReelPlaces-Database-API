5d1c87ad7ae9373f53794bbfdcf0359eb5f4fcac
Chris <c.bailey00@hotmail.com>
2023-02-24 16:02:17 +0000

    added port env variable to index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
 const routes = require('./routes/routes')
 const cors = require('cors')
+const port = (process.env.PORT || 3000)
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -22,6 +23,6 @@ app.use(cors())
 app.use(express.json());
 app.use('/api', routes)
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
 })
