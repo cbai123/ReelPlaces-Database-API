8a8a503f1c6e8b891e6eef20aeb2c1e295607a83
Chris <c.bailey00@hotmail.com>
2023-02-24 15:26:47 +0000

    added imdb api calls

--- routes/routes.js
+++ routes/routes.js
@@ -16,4 +16,22 @@ router.get('/getOne/:id', async (req, res) => {
   }
 })
 
+//IMDB API requests
+router.get('/imdb/trending', async (req, res) => {
+  try {
+    const response = await fetch(`https://imdb-api.com/en/API/InTheaters/${process.env.IMDB_KEY}`)
+    const result = await response.json()
+    res.json(result)
+  }
+  catch(error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+router.get('/imdb/byLocation/:location', async (req, res) => {
+  const response = await fetch(`https://imdb-api.com/API/AdvancedSearch/${process.env.IMDB_KEY}?has=locations&locations=${req.params.location}`)
+  const result = await response.json()
+  res.json(result)
+})
+
 module.exports = router;
